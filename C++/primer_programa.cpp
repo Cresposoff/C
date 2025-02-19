3108550eec26328e75d2aff0cd74f79bd442ebfb
crespitosoff <u20241221018@usco.edu.co>
2025-02-19 08:48:08 -0500

    primer

---
+++ C++/primer_programa.cpp
@@ -0,0 +1,9 @@
+
+#include <iostream>
+
+int main() {
+    std::cout << "¡Hola, mundo! Este es mi primer programa en C++." << std::endl;
+    std::cin.get(); // Espera a que presiones Enter
+    return 0;
+}
+
